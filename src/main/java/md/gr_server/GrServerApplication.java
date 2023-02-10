76eee550748ca639698afa453b42344e7eaa2d10
Roman Rudi <roman.rudi@outlook.com>
2023-02-10 08:45:17 +0200

    Init GrServerApplication, connect to db

---
+++ src/main/java/md/gr_server/GrServerApplication.java
@@ -0,0 +1,13 @@
+package md.gr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrServerApplication.class, args);
+	}
+
+}
